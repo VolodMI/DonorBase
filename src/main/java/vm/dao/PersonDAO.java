ee3b30e5807e173d0e04202a7bb24b9872310c1d
VolodMI <volodymyr.marchuk3004@gmail.com>
2022-07-27 00:16:52 +0300

    update

--- src/main/java/vm/dao/PersonDAO.java
+++ src/main/java/vm/dao/PersonDAO.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import vm.models.Person;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Component
